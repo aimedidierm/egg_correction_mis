623384087dc7cbaeb34a4808fa072a7e4c88fbdc
aimedidierm <aimedidiermugisha@gmail.com>
2022-08-15 12:23:46 +0200

    sales

---
+++ farmer/sales.php
@@ -0,0 +1,131 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+require '../php-includes/connect.php';
+require 'php-includes/check-login.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Egg correction - sales
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="">
+<?php require 'php-includes/nav.php';?>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-plain">
+              <div class="card-header">
+                <h4 class="card-title"> Buyers list</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>
+                        Names
+                      </th>
+                      <th>
+                        Email
+                      </th>
+                      <th>
+                        Phone
+                      </th>
+                      <th>
+                        Quantity
+                      </th>
+                      <th>
+                        Address
+                      </th>
+                      <th>
+                      </th>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $sql = "SELECT * FROM buyers_mess";
+                    $stmt = $db->prepare($sql);
+                    $stmt->execute();
+                    if ($stmt->rowCount() > 0) {
+                        $count = 1;
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                      <tr>
+                        <td>
+                        <?php echo $row['names'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['email'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['phone'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['quantity'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['address'];?>
+                        </td>
+                        <td>
+                        <form method="post"><button type="submit" class="btn btn-success" id="<?php echo $row["id"];$sid=$row["id"]; ?>" name="delete">Comfirm</button></form>
+                        </td>
+                      </tr>
+                      <?php
+                        $count++;
+                        }
+                    }
+                    if(isset($_POST['delete'])){
+                        $sql ="DELETE FROM buyers_mess WHERE id = ?";
+                        $stm = $db->prepare($sql);
+                        if ($stm->execute(array($sid))) {
+                            print "<script>alert('Order removed');window.location.assign('sales.php')</script>";
+                
+                        } else {
+                            print "<script>alert('Fail');window.location.assign('sales.php')</script>";
+                        }
+                    }
+                    ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+  <script src="../assets/demo/demo.js"></script>
+</body>
+
+</html>
