ed12208bed1a513858b0aad614f2753036d8f39d
aimedidierm <aimedidiermugisha@gmail.com>
2022-08-15 14:18:10 +0200

    farmer pages

---
+++ farmer/store.php
@@ -0,0 +1,174 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+require '../php-includes/connect.php';
+require 'php-includes/check-login.php';
+$query = "SELECT * FROM farmer WHERE email= ? limit 1";
+$stmt = $db->prepare($query);
+$stmt->execute(array($_SESSION['email']));
+$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($stmt->rowCount()>0) {
+    $farmer=$rows['id'];
+    $totaleggs=$rows['eggs'];
+}
+if(isset($_POST['remove'])){
+    $amount=$_POST['eggs'];
+    $newbalance=$totaleggs-$amount;
+    if ($amount <= $totaleggs){
+        $sql ="UPDATE farmer SET eggs = ? WHERE email = ? limit 1";
+        $stm = $db->prepare($sql);
+        if ($stm->execute(array($newbalance, $_SESSION['email']))) {
+            $sql ="INSERT INTO eggs (farmer,credit,total) VALUES (?,?,?)";
+            $stm = $db->prepare($sql);
+            $stm->execute(array($farmer,$amount,$newbalance));
+            print "<script>alert('Eggs removed');window.location.assign('store.php')</script>";
+    
+        }
+    } else{
+        echo "<script>alert('You have low number');window.location.assign('store.php')</script>";
+    }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Egg correction - store
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="">
+<?php require 'php-includes/nav.php';?>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-12">
+          <div class="row">
+            <div class="card card-user">
+              <div class="card-header">
+                <h5 class="card-title">Total: <?php echo $totaleggs;?> Eggs</h5>
+              </div>
+              <div class="card-body">
+                <form method="post">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label>Enter number</label>
+                        <input type="number" class="form-control" name="eggs">
+                    <div class="update ml-auto mr-auto">
+                      <button type="submit" name="remove" class="btn btn-primary btn-round">remove eggs</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+            <div class="card card-plain">
+              <div class="card-header">
+                <h4 class="card-title">Layed eggs</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>
+                        Debit
+                      </th>
+                      <th>
+                        Credit
+                      </th>
+                      <th>
+                        Total
+                      </th>
+                      <th>
+                        Time
+                      </th>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $query = "SELECT * FROM farmer WHERE email= ? limit 1";
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array($_SESSION['email']));
+                    $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+                    if ($stmt->rowCount()>0) {
+                        $id=$rows['id'];
+                    }
+                    $sql = "SELECT * FROM eggs WHERE farmer=?";
+                    $stmt = $db->prepare($sql);
+                    $stmt->execute(array($id));
+                    if ($stmt->rowCount() > 0) {
+                        $count = 1;
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                      <tr>
+                        <td>
+                        <?php echo $row['debit'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['credit'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['total'];?>
+                        </td>
+                        <td>
+                        <?php echo $row['time'];?>
+                        </td>
+                      </tr>
+                      <?php
+                        $count++;
+                        }
+                    }
+                    if(isset($_POST['delete'])){
+                        $sql ="DELETE FROM buyers_mess WHERE id = ?";
+                        $stm = $db->prepare($sql);
+                        if ($stm->execute(array($sid))) {
+                            print "<script>alert('Order removed');window.location.assign('sales.php')</script>";
+                
+                        } else {
+                            print "<script>alert('Fail');window.location.assign('sales.php')</script>";
+                        }
+                    }
+                    ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+  <script src="../assets/demo/demo.js"></script>
+</body>
+
+</html>
