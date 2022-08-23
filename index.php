7049fb3169d84aa3975bb0df1dc6898fc6fa9a2c
aimedidierm <aimedidiermugisha@gmail.com>
2022-08-23 12:54:34 +0200

    Shop front end

--- index.php
+++ index.php
@@ -1,40 +1,69 @@
-
 <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Simple Chat System using AJAX</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<style>
-	#login_form{
-		width:350px;
-		height:350px;
-		position:relative;
-		top:50px;
-		margin: auto;
-		padding: auto;
-	}
-</style>
-</head>
-<body>
-<div class="container">
-	<div id="login_form" class="well">
-		<h2><center><span class="glyphicon glyphicon-lock"></span> Please Login</center></h2>
-		<hr>
-		<form method="POST" action="login.php">
-		Username: <input type="text" name="email" class="form-control" required>
-		<div style="height: 10px;"></div>		
-		Password: <input type="password" name="password" class="form-control" required> 
-		<div style="height: 10px;"></div>
-		<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Login</button>
-		</form>
-		<div style="height: 15px;"></div>
-		<div style="color: red; font-size: 15px;">
-			<center>
-						</center>
-		</div>
-	</div>
-</div>
-</body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Akazu system</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="homepage/assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="homepage/css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Akazu system</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <form class="d-flex">
+                        <a href="sign_in.php" class="btn btn-outline-dark" type="submit">Login</a>
+                        
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Akazu system</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">shop with us</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="homepage/img/eggs.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Rwakayiru</h5>
+                                    <!-- Product price-->
+                                    10 Eggs
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Buy now</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="homepage/js/scripts.js"></script>
+    </body>
 </html>
