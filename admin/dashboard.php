72ceb302e0ed0e1c393c50929c03909234c2cfe0
aimedidierm <aimedidiermugisha@gmail.com>
2022-09-22 16:41:31 +0200

    dashbord frontend

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -31,8 +31,131 @@ require 'php-includes/check-login.php';
   <?php require 'php-includes/nav.php';?>
       <!-- End Navbar -->
       <div class="content">
-
-      </div>
+      <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+              <div class="card-body ">
+                <div class="row">
+                  <div class="col-5 col-md-4">
+                    <div class="icon-big text-center icon-warning">
+                      <i class="nc-icon nc-box-2 text-warning"></i>
+                    </div>
+                  </div>
+                  <div class="col-7 col-md-8">
+                    <div class="numbers">
+                      <p class="card-category">Total eggs</p>
+                      <p class="card-title">25<p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-refresh"></i>
+                  Update Now
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+              <div class="card-body ">
+                <div class="row">
+                  <div class="col-5 col-md-4">
+                    <div class="icon-big icon-warning">
+                      <i class="nc-icon nc-email-85 text-success"></i>
+                    </div>
+                  </div>
+                  <div class="col-7 col-md-8">
+                    <div class="numbers">
+                      <p class="card-category">Help messeges</p>
+                      <p class="card-title">5<p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-calendar-o"></i>
+                  Last day
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+              <div class="card-body ">
+                <div class="row">
+                  <div class="col-5 col-md-4">
+                    <div class="icon-big text-center icon-warning">
+                      <i class="nc-icon nc-istanbul text-danger"></i>
+                    </div>
+                  </div>
+                  <div class="col-7 col-md-8">
+                    <div class="numbers">
+                      <p class="card-category">Total stores</p>
+                      <p class="card-title">23<p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-clock-o"></i>
+                  In the last hour
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+              <div class="card-body ">
+                <div class="row">
+                  <div class="col-5 col-md-4">
+                    <div class="icon-big text-center icon-warning">
+                      <i class="nc-icon nc-circle-10 text-primary"></i>
+                    </div>
+                  </div>
+                  <div class="col-7 col-md-8">
+                    <div class="numbers">
+                      <p class="card-category">Customers</p>
+                      <p class="card-title">+45K<p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-refresh"></i>
+                  Update now
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="row">
+          <div class="col-md-12">
+            <div class="card ">
+              <div class="card-header ">
+                <h5 class="card-title">Layed eggs</h5>
+                <p class="card-category">24 Hours performance</p>
+              </div>
+              <div class="card-body ">
+                <canvas id=chartHours width="400" height="100"></canvas>
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-history"></i> Updated 3 minutes ago
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
   <!--   Core JS Files   -->
--- db/egg_correction.sql
+++ db/egg_correction.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 15, 2022 at 03:40 PM
+-- Generation Time: Sep 22, 2022 at 04:40 PM
 -- Server version: 8.0.29-0ubuntu0.20.04.3
 -- PHP Version: 7.4.3
 
@@ -48,6 +48,30 @@ INSERT INTO `admin` (`id`, `email`, `names`, `phone`, `address`, `password`, `ti
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `akazu`
+--
+
+CREATE TABLE `akazu` (
+  `id` int NOT NULL,
+  `farmer` int NOT NULL,
+  `debit` int DEFAULT NULL,
+  `credit` int DEFAULT NULL,
+  `total` int NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `akazu`
+--
+
+INSERT INTO `akazu` (`id`, `farmer`, `debit`, `credit`, `total`, `time`) VALUES
+(1, 1, 10, 0, 10, '2022-08-15 13:39:45'),
+(2, 1, NULL, 1, 9, '2022-08-15 14:12:53'),
+(3, 1, NULL, 2, 7, '2022-08-15 14:14:03');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `buyers_mess`
 --
 
@@ -58,6 +82,7 @@ CREATE TABLE `buyers_mess` (
   `email` varchar(255) NOT NULL,
   `quantity` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
+  `farmer` int NOT NULL,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -65,8 +90,10 @@ CREATE TABLE `buyers_mess` (
 -- Dumping data for table `buyers_mess`
 --
 
-INSERT INTO `buyers_mess` (`id`, `names`, `phone`, `email`, `quantity`, `address`, `time`) VALUES
-(1, 'Didier', '07887509789', 'aimedidiermugisha@gmail.com', '23', 'Nyanza, Rwanda', '2022-08-15 12:21:11');
+INSERT INTO `buyers_mess` (`id`, `names`, `phone`, `email`, `quantity`, `address`, `farmer`, `time`) VALUES
+(1, 'Didier', '0788750979', 'aimedidiermugisha@gmail.com', '2', 'Nyanza', 2, '2022-09-13 07:32:43'),
+(2, 'Didier', '0788', 'admin@mgail.com', '1', 'Nyanza', 1, '2022-09-21 08:51:25'),
+(3, 'Didier', '0788750979', 'admin@gmail.com', '31', 'Down town', 1, '2022-09-21 08:51:50');
 
 -- --------------------------------------------------------
 
@@ -90,7 +117,16 @@ CREATE TABLE `eggs` (
 INSERT INTO `eggs` (`id`, `farmer`, `debit`, `credit`, `total`, `time`) VALUES
 (1, 1, 10, 0, 10, '2022-08-15 13:39:45'),
 (2, 1, NULL, 1, 9, '2022-08-15 14:12:53'),
-(3, 1, NULL, 2, 7, '2022-08-15 14:14:03');
+(3, 1, NULL, 2, 7, '2022-08-15 14:14:03'),
+(4, 1, 1, NULL, 10, '2022-08-23 11:13:58'),
+(5, 1, NULL, 2, 8, '2022-08-23 11:27:56'),
+(6, 1, 1, NULL, 10, '2022-08-23 11:28:06'),
+(7, 1, 1, NULL, 12, '2022-08-23 11:32:36'),
+(8, 1, 1, NULL, 14, '2022-08-23 11:50:52'),
+(9, 1, 1, NULL, 17, '2022-08-23 11:51:12'),
+(10, 1, 1, NULL, 19, '2022-08-23 11:52:01'),
+(11, 1, 1, NULL, 20, '2022-08-23 11:52:18'),
+(12, 1, 2, NULL, 22, '2022-08-23 11:53:52');
 
 -- --------------------------------------------------------
 
@@ -114,7 +150,95 @@ CREATE TABLE `farmer` (
 --
 
 INSERT INTO `farmer` (`id`, `email`, `names`, `phone`, `address`, `eggs`, `password`, `time`) VALUES
-(1, 'farmer@gmail.com', 'Aime DIdier ', '0788750979', 'Huye, Rwanda', 7, '3b081fd5426c134088a9b1466ff4c224', '2022-06-17 13:58:07');
+(1, 'farmer@gmail.com', 'Aime DIdier ', '0788750979', 'Huye, Rwanda', 22, '3b081fd5426c134088a9b1466ff4c224', '2022-06-17 13:58:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `names` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`id`, `email`, `names`, `phone`, `address`, `time`) VALUES
+(1, 'food@gmail.com', 'Aime DIdier ', '0788750979', 'Huye, Rwanda', '2022-06-17 13:58:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `help`
+--
+
+CREATE TABLE `help` (
+  `id` int NOT NULL,
+  `send` enum('0','1') NOT NULL,
+  `mess` text NOT NULL,
+  `farmer` int NOT NULL,
+  `admin` int NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `help`
+--
+
+INSERT INTO `help` (`id`, `send`, `mess`, `farmer`, `admin`, `time`) VALUES
+(6, '0', 'hello', 1, 1, '2022-09-22 12:31:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tec`
+--
+
+CREATE TABLE `tec` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `names` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tec`
+--
+
+INSERT INTO `tec` (`id`, `email`, `names`, `phone`, `address`, `time`) VALUES
+(1, 'tec@gmail.com', 'Aime DIdier ', '0788750979', 'Huye, Rwanda', '2022-06-17 13:58:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vet`
+--
+
+CREATE TABLE `vet` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `names` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `vet`
+--
+
+INSERT INTO `vet` (`id`, `email`, `names`, `phone`, `address`, `time`) VALUES
+(1, 'vet@gmail.com', 'Aime DIdier ', '0788750979', 'Huye, Rwanda', '2022-06-17 13:58:07');
 
 --
 -- Indexes for dumped tables
@@ -128,10 +252,17 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `akazu`
+--
+ALTER TABLE `akazu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `buyers_mess`
 --
 ALTER TABLE `buyers_mess`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `farmer` (`farmer`);
 
 --
 -- Indexes for table `eggs`
@@ -147,6 +278,35 @@ ALTER TABLE `farmer`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `help`
+--
+ALTER TABLE `help`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `farmer` (`farmer`),
+  ADD KEY `admin` (`admin`);
+
+--
+-- Indexes for table `tec`
+--
+ALTER TABLE `tec`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `vet`
+--
+ALTER TABLE `vet`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -157,22 +317,52 @@ ALTER TABLE `admin`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `akazu`
+--
+ALTER TABLE `akazu`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `buyers_mess`
 --
 ALTER TABLE `buyers_mess`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `eggs`
 --
 ALTER TABLE `eggs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `farmer`
 --
 ALTER TABLE `farmer`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `help`
+--
+ALTER TABLE `help`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tec`
+--
+ALTER TABLE `tec`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `vet`
+--
+ALTER TABLE `vet`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
