fcc6564880df5488aef008ad084e30b4458b4dc7
aimedidierm <aimedidiermugisha@gmail.com>
2022-12-22 02:01:40 +0200

    Egg to web

--- hardware/full/full.ino
+++ hardware/full/full.ino
@@ -5,14 +5,24 @@
 SoftwareSerial s(3,1);
 const char* ssid = "Virus";
 const char* password = "mbega123455";
-//String serverName = "http://137.184.232.255/smart_public_tape/data.php";
+//String serverName = "http://137.184.232.255/akazu_system/data.php";
 //String serverName = "http://didier.requestcatcher.com/";
+String serverName = "http://192.168.43.76/akazu_system/data.php";
+const int trigPin = D2;
+const int echoPin = D4;
 
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
 
-String serverName = "http://192.168.43.76/smart_home/dta.php";
+long duration;
+float distanceCm;
+float distanceInch;
+
 void setup() {
-  s.begin(9600);
-  Serial.begin(9600);
+  Serial.begin(9600); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) {
   delay(500);
@@ -20,13 +30,25 @@ void setup() {
 }
 
 void loop() {
-    if(s.available( ) > 0){
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_VELOCITY/2;
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  if(distanceCm < 38){
       WiFiClient client;
       HTTPClient http;
       http.begin(client, serverName);
-      //http.addHeader("Content-Type", "application/json");
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      String httpRequestData = s.readStringUntil('\n');
+      String httpRequestData = "egg";
       int      httpResponseCode = http.POST(httpRequestData);
       if (httpResponseCode>0) {
         s.println(httpResponseCode);
@@ -36,4 +58,5 @@ void loop() {
       }
       http.end();
     }
-  }
+  delay(100);
+}
--- sms.php
+++ sms.php
@@ -2,19 +2,8 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
-require 'php-includes/connect.php';
 use Yvesniyo\IntouchSms\SmsSimple;
-if(isset($_POST['egg'])){
-    $query = "SELECT * FROM akazu ORDER BY id DESC limit 1";
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $rows = $stmt->fetch(PDO::FETCH_ASSOC);
-    $total=$rows['total'];
-    $total=$total+1;
-    $sql ="INSERT INTO akazu (farmer,debit,credit,total) VALUES ('1','1','0',?)";
-    $stm = $db->prepare($sql);
-    $stm->execute(array($total));
-    include_once("vendor/autoload.php");
+include_once("vendor/autoload.php");
     /** @var \Yvesniyo\IntouchSms\SmsSimple */
     $messi="New egg detected";
     $sms = new SmsSimple();
@@ -26,5 +15,4 @@ if(isset($_POST['egg'])){
         ->apiUrl("www.intouchsms.co.rw/api/sendsms/.json")
         ->callBackUrl("");
     print_r($sms->send());
-}
 ?>
